75749efd11d8ca250cc5fd5948dcb77802818dfa
Ryan McClure <top.gear_@live.co.uk>
2020-05-16 15:44:40 +0100

    remove auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,14 +14,17 @@ Rails.application.routes.draw do
                   registrations: 'registrations'
                 }
 
-  # devise_scope :user,
-  #
-  #   post "/login", to: "auth#login"
-  #   get "/auto_login", to: "auth#auto_login"
 
+  devise_scope :user do
+
+  post "/login", to: "sessions#create"
+  get "/signup", to: "registrations#new"
+  get "/auto_login", to: "sessions#new"
+end
   # post "/login", to: "auth#login"
   # get "/auto_login", to: "auth#auto_login"
   # get "/user_is_authed", to: "auth#user_is_authed"
+  root to: "home#index"
 end
 end
 end
