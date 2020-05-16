8cb2e40af94987ae46c73366575cc4999d54075a
Ryan McClure <top.gear_@live.co.uk>
2020-05-16 15:23:51 +0100

    Set up a revocation strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    has_secure_password
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 end
